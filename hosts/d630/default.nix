d7428c2aaebd758c23c40c363ac55e1530e026aa
Actoriu <864149939@qq.com>
2022-07-18 22:22:07 +0800

    update

--- hosts/d630/default.nix
+++ hosts/d630/default.nix
@@ -93,73 +93,73 @@
       package = pkgs.zsh;
     };
     user.name = "actoriu";
-    version.enable = "22.05"
-      video = {
-    mpv.enable = true;
+    version.enable = "22.05";
+    video = {
+      mpv.enable = true;
+    };
   };
-};
 
-users.users.${config.user.name} = {
-isNormalUser = true;
-extraGroups = [ "wheel" ];
-uid = 1000;
-useDefaultShell = true;
-};
+  users.users.${config.user.name} = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+    uid = 1000;
+    useDefaultShell = true;
+  };
 
-home-manager.users.${config.user.name} = {
-home.stateVersion = config.custom.version;
-};
+  home-manager.users.${config.user.name} = {
+    home.stateVersion = config.custom.version;
+  };
 
-# console = {
-#   font = "Lat2-Terminus16";
-#   keyMap = "us";
-#   useXkbConfig = true; # use xkbOptions in tty.
-# };
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  #   useXkbConfig = true; # use xkbOptions in tty.
+  # };
 
-# Configure keymap in X11
-# services.xserver.layout = "us";
-# services.xserver.xkbOptions = {
-#   "eurosign:e";
-#   "caps:escape" # map caps to escape.
-# };
+  # Configure keymap in X11
+  # services.xserver.layout = "us";
+  # services.xserver.xkbOptions = {
+  #   "eurosign:e";
+  #   "caps:escape" # map caps to escape.
+  # };
 
-# List packages installed in system profile. To search, run:
-# $ nix search wget
-# environment.systemPackages = with pkgs; [
-#   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-#   wget
-#   firefox
-# ];
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  # environment.systemPackages = with pkgs; [
+  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #   wget
+  #   firefox
+  # ];
 
-# Some programs need SUID wrappers, can be configured further or are
-# started in user sessions.
-# programs.mtr.enable = true;
-# programs.gnupg.agent = {
-#   enable = true;
-#   enableSSHSupport = true;
-# };
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
 
-# List services that you want to enable:
+  # List services that you want to enable:
 
-# Enable the OpenSSH daemon.
-# services.openssh.enable = true;
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
 
-# Open ports in the firewall.
-# networking.firewall.allowedTCPPorts = [ ... ];
-# networking.firewall.allowedUDPPorts = [ ... ];
-# Or disable the firewall altogether.
-# networking.firewall.enable = false;
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
 
-# Copy the NixOS configuration file and link it from the resulting system
-# (/run/current-system/configuration.nix). This is useful in case you
-# accidentally delete configuration.nix.
-# system.copySystemConfiguration = true;
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
 
-# This value determines the NixOS release from which the default
-# settings for stateful data, like file locations and database versions
-# on your system were taken. It‘s perfectly fine and recommended to leave
-# this value at the release version of the first install of this system.
-# Before changing this value read the documentation for this option
-# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-# system.stateVersion = "22.05"; # Did you read the comment?
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  # system.stateVersion = "22.05"; # Did you read the comment?
 }
