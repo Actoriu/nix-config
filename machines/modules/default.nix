1c3f98dd78d3b5a819b07cfe03a2049ec5f2f260
Actoriu <864149939@qq.com>
2022-07-24 10:05:51 +0800

    update

--- machines/modules/default.nix
+++ machines/modules/default.nix
@@ -1,12 +1,27 @@
-{ ... }: {
-  imports = [
-    ./fonts
-    ./hardware
-    ./locale
-    ./loader
-    ./network
-    ./power-management
-    ./usershell
-    ./version
-  ];
+# { ... }: {
+#   imports = [
+#     ./fonts
+#     ./hardware
+#     ./locale
+#     ./loader
+#     ./network
+#     ./power-management
+#     ./usershell
+#     ./version
+#   ];
+# }
+
+{ pkgs
+, lib
+, ...
+}:
+
+let
+  folder = ./.;
+  toImport = name: value: folder + ("/" + name);
+  filterCaches = key: value: value == "regular" && lib.hasSuffix ".nix" key && key != "default.nix";
+  imports = lib.mapAttrsToList toImport (lib.filterAttrs filterCaches (builtins.readDir folder));
+in
+{
+  inherit imports;
 }
