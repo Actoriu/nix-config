e99ce6ab3380f780d9c25b3201b645037250fca8
Actoriu <864149939@qq.com>
2022-08-01 22:17:58 +0800

    update

--- profiles/users/home-manager/default.nix
+++ profiles/users/home-manager/default.nix
@@ -2,8 +2,8 @@
 , ...
 }: {
   home = {
-    username = config.users.users.${name}.name;
-    homeDirectory = config.users.users.${name}.home;
+    username = config.custom.users.user.name;
+    homeDirectory = "home/${config.custom.users.user.name}";
     stateVersion = config.custom.users.version;
   };
   programs.home-manager.enable = true;
