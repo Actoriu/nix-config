808e186c502007c0081bd3004fa0263f1d456a01
Actoriu <864149939@qq.com>
2022-08-03 21:18:38 +0800

    update

--- flake.nix
+++ flake.nix
@@ -185,6 +185,7 @@
   #   ];
   # }
   // {
+    (
     inputs.flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
     let
     pkgs = inputs.nixos.legacyPackages.${system};
@@ -209,34 +210,34 @@
       };
     };
   });
-}
+  )}
   // inputs.flake-utils.lib.eachSystem [ "aarch64-linux" "x86_64-linux" ] (system:
   {
-    devShells =
-      let pkgs = import inputs.nixos {
-        inherit system;
-        overlays = [ inputs.devshell.overlay ];
+  devShells =
+    let pkgs = import inputs.nixos {
+      inherit system;
+      overlays = [ inputs.devshell.overlay ];
+    };
+    in
+    {
+      default = pkgs.devshell.mkShell {
+        name = "nix-config";
+        imports = [ (pkgs.devshell.extraModulesDir + "/git/hooks.nix") ];
+        git.hooks.enable = true;
+        git.hooks.pre-commit.text = "${pkgs.treefmt}/bin/treefmt";
+        packages = with pkgs; [
+          cachix
+          nix-build-uncached
+          nixpkgs-fmt
+          nodePackages.prettier
+          nodePackages.prettier-plugin-toml
+          shfmt
+          treefmt
+        ];
+        devshell.startup.nodejs-setuphook = pkgs.lib.stringsWithDeps.noDepEntry ''
+          export NODE_PATH=${pkgs.nodePackages.prettier-plugin-toml}/lib/node_modules:$NODE_PATH
+        '';
       };
-      in
-      {
-        default = pkgs.devshell.mkShell {
-          name = "nix-config";
-          imports = [ (pkgs.devshell.extraModulesDir + "/git/hooks.nix") ];
-          git.hooks.enable = true;
-          git.hooks.pre-commit.text = "${pkgs.treefmt}/bin/treefmt";
-          packages = with pkgs; [
-            cachix
-            nix-build-uncached
-            nixpkgs-fmt
-            nodePackages.prettier
-            nodePackages.prettier-plugin-toml
-            shfmt
-            treefmt
-          ];
-          devshell.startup.nodejs-setuphook = pkgs.lib.stringsWithDeps.noDepEntry ''
-            export NODE_PATH=${pkgs.nodePackages.prettier-plugin-toml}/lib/node_modules:$NODE_PATH
-          '';
-        };
-      };
-  });
+    };
+});
 }
