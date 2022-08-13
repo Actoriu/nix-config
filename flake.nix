7d759cd9afe62158a35cf56b8ea97de075e5f23b
Actoriu <864149939@qq.com>
2022-08-13 17:43:50 +0800

    update

--- flake.nix
+++ flake.nix
@@ -153,7 +153,7 @@
               ];
             };
           })
-          ../../user/actoriu/default.nix
+          ./user/actoriu
         ];
       };
       # packages.x86_64-linux.homeConfigurations.actoriu.activationPackage;
@@ -184,7 +184,7 @@
   #               ];
   #             };
   #           })
-  #           ../../user/actoriu
+  #           ./user/actoriu
   #         ];
   #       };
   #     };
