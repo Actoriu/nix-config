b2b37717a0eae77cc9c7ebd50bce4ffe1ac78c7d
Actoriu <864149939@qq.com>
2022-09-05 22:38:49 +0800

    update default.nix and shell.nix

--- default.nix
+++ default.nix
@@ -1,13 +1,4 @@
-{ pkgs
-, ...
-}: {
-  modules = import ./modules/nixos;
-  homeManagerModules = import ./modules/home-manager;
-}
-// (import ./pkgs { inherit pkgs; })
-  // (
-  if builtins ? getFlake then
-    builtins.getFlake (toString ./.)
-  else
-    (import ./lib/compat).defaultNix
-)
+if builtins ? getFlake then
+  builtins.getFlake (toString ./.)
+else
+  (import ./lib/compat).defaultNix
