937986ec27bc6d5b897a624cab1116f8d7eb8b0d
Actoriu <864149939@qq.com>
2022-09-18 09:56:56 +0800

    update

--- hosts/oneplus5/default.nix
+++ hosts/oneplus5/default.nix
@@ -1,14 +1,7 @@
 { config
 , pkgs
 , ...
-}:
-let
-  sshdTmpDirectory = "${config.user.home}/sshd-tmp";
-  sshdDirectory = "${config.user.home}/sshd";
-  pathToPubKey = "${config.user.home}/.ssh/id_rsa.pub";
-  port = 8022;
-in
-{
+}: {
   nix = {
     extraOptions = ''
       experimental-features = nix-command flakes
@@ -35,14 +28,6 @@ in
       # User-facing stuff that you really really want to have
       # vim  # or some other editor, e.g. nano or neovim
 
-      # sshd-start script
-      # refers to: https://github.com/t184256/nix-on-droid/wiki/SSH-access
-      (pkgs.writeScriptBin "sshd-start" ''
-        #!${pkgs.runtimeShell}
-        echo "Starting sshd in non-daemonized way on port ${toString port}"
-        ${pkgs.openssh}/bin/sshd -f "${sshdDirectory}/sshd_config" -D
-      '')
-
       # Some common stuff that people expect to have
       diffutils
       findutils
@@ -62,11 +47,6 @@ in
       unzip
       openssh
       nettools
-      (
-        lib.setPrio # make bintools less prior
-          (busybox.meta.priority + 10)
-          busybox
-      )
     ];
 
     # Backup etc files instead of failing to activate generation if a file already exists in /etc
@@ -76,38 +56,24 @@ in
   # Read the changelog before changing this value
   system.stateVersion = "22.05";
 
-  build.activation.sshd = ''
-    $DRY_RUN_CMD mkdir $VERBOSE_ARG --parents "${config.user.home}/.ssh"
-    $DRY_RUN_CMD cat ${pathToPubKey} > "${config.user.home}/.ssh/authorized_keys"
-    if [[ ! -d "${sshdDirectory}" ]]; then
-      $DRY_RUN_CMD rm $VERBOSE_ARG --recursive --force "${sshdTmpDirectory}"
-      $DRY_RUN_CMD mkdir $VERBOSE_ARG --parents "${sshdTmpDirectory}"
-      $VERBOSE_ECHO "Generating host keys..."
-      $DRY_RUN_CMD ${pkgs.openssh}/bin/ssh-keygen -t rsa -b 4096 -f "${sshdTmpDirectory}/ssh_host_rsa_key" -N ""
-      $VERBOSE_ECHO "Writing sshd_config..."
-      $DRY_RUN_CMD echo -e "HostKey ${sshdDirectory}/ssh_host_rsa_key\nPort ${toString port}\n" > "${sshdTmpDirectory}/sshd_config"
-      $DRY_RUN_CMD mv $VERBOSE_ARG "${sshdTmpDirectory}" "${sshdDirectory}"
-    fi
-  '';
-
-  # nix-channel --add https://github.com/rycee/home-manager/archive/release-22.05.tar.gz home-manager
+  # nix-channel --add https://github.com/rycee/home-manager/archive/release-22.11.tar.gz home-manager
   # nix-channel --update
   # you can configure home-manager in here like
-  home-manager = {
-    useGlobalPkgs = true;
-    useUserPackages = true;
-    config =
-      { lib
-      , pkgs
-      , ...
-      }: {
-        home.stateVersion = "22.11";
-        imports = [
-          ../../users/modules
-          ../../users/nix-on-droid
-        ];
-      };
-  };
+  # home-manager = {
+  #   useGlobalPkgs = true;
+  #   useUserPackages = true;
+  #   config =
+  #     { lib
+  #     , pkgs
+  #     , ...
+  #     }: {
+  #       home.stateVersion = "22.11";
+  #       imports = [
+  #         ../../users/modules
+  #         ../../users/nix-on-droid
+  #       ];
+  #     };
+  # };
 }
 # vim: ft=nix
 
