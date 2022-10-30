67c3a749d587ce0054eb0086d39f6b1e425b9fc9
Actoriu <864149939@qq.com>
2022-10-30 17:32:36 +0800

    update

---
+++ lib/home-manager.nix
@@ -0,0 +1,38 @@
+{ inputs
+, ...
+}:
+
+let
+  inherit (inputs) self home-manager;
+  inherit (home-manager.lib) homeManagerConfiguration;
+in
+{
+  mkHome =
+    { hostname ? null
+    , username
+    , system ? "x86_64-linux"
+    , pkgs
+    , extraModules ? [ ]
+    , sharedModules ? [
+        {
+          home = {
+            inherit username;
+            homeDirectory = "home/${username}";
+            stateVersion = "22.11";
+          };
+          programs.home-manager.enable = true;
+          manual.manpages.enable = false;
+          systemd.user.startServices = "sd-switch";
+        }
+      ]
+    , persistence ? false
+    , ...
+    }:
+    homeManagerConfiguration {
+      inherit pkgs;
+      extraSpecialArgs = {
+        inherit inputs self hostname username persistence;
+      };
+      modules = extraModules ++ sharedModules ++ [ ../users/${username} ];
+    };
+}
