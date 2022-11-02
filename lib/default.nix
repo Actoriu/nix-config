95b894eea57898619ed0dc62a3e0946855a6a5a7
Actoriu <864149939@qq.com>
2022-11-02 19:00:52 +0800

    add nixpkgs.lib.extend

--- lib/default.nix
+++ lib/default.nix
@@ -5,8 +5,8 @@
 
 {
   my = {
-    mkNixOS = import mkNixOS.nix { inherit inputs lib; };
-    mkHome = import mkHome.nix { inherit inputs lib; };
-    mkDroid = import mkDroid { inherit inputs lib; };
+    mkNixOS = import ./mkNixOS.nix { inherit inputs lib; };
+    mkHome = import ./mkHome.nix { inherit inputs lib; };
+    mkDroid = import ./mkDroid { inherit inputs lib; };
   };
 }
