771836aec2178daf03dc85f47af032c50fca0a8a
Actoriu <864149939@qq.com>
2022-11-05 17:18:47 +0800

    update

--- lib/default.nix
+++ lib/default.nix
@@ -4,19 +4,31 @@
 , ...
 }:
 
+# let
+#   inherit (lib) makeExtensible attrValues foldr;
+#   inherit (modules) mapModules;
+
+#   modules = import ./modules.nix {
+#     inherit lib;
+#     self.attrs = import ./attrs.nix { inherit lib; self = { }; };
+#   };
+
+#   mylib = makeExtensible (self:
+#     with self; mapModules ./.
+#       (file: import file { inherit self lib pkgs inputs; }));
+# in
+# mylib.extend
+#   (self: super:
+#     foldr (a: b: a // b) { } (attrValues super))
 let
-  inherit (lib) makeExtensible attrValues foldr;
-  inherit (modules) mapModules;
-
-  modules = import ./modules.nix {
-    inherit lib;
-    self.attrs = import ./attrs.nix { inherit lib; self = { }; };
-  };
-
-  mylib = makeExtensible (self:
-    with self; mapModules ./.
-      (file: import file { inherit self lib pkgs inputs; }));
+  mylib = lib.makeExtensible (self:
+    let
+      callLibs = file: import file { lib = self; };
+    in
+    {
+      droid = callLibs ./droid.nix;
+      home-manager = callLibs ./home-manager.nix;
+      nixos = callLibs ./nixos;
+    });
 in
-mylib.extend
-  (self: super:
-    foldr (a: b: a // b) { } (attrValues super))
+mylib
