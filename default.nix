5a3bf7a1a4c6b63cc408f5fa10db9f2864bc18f1
Actoriu <864149939@qq.com>
2022-11-06 14:55:53 +0800

    update

--- default.nix
+++ default.nix
@@ -1,15 +1,15 @@
 if builtins ? getFlake then
   builtins.getFlake (toString ./.)
 else
-  (import (
+  (
     let
-      lock = builtins.fromJSON (builtins.readFile ../../flake.lock);
+      lock = builtins.fromJSON (builtins.readFile ./flake.lock);
       inherit (lock.nodes.flake-compat.locked) owner repo rev narHash;
       flake-compat = fetchTarball {
         url = "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz";
         sha256 = narHash;
       };
-      flake = import flake-compat { src = ../../.; };
+      flake = import flake-compat { src = ./.; };
     in
     flake
-  )).defaultNix
+  ).defaultNix
