104a35255b19ba02db5c149112e2a4522bf857c6
Actoriu <864149939@qq.com>
2022-11-13 14:12:29 +0800

    update

--- lib/default.nix
+++ lib/default.nix
@@ -4,28 +4,19 @@
 , ...
 }:
 
-{
-  imports =
-    let
-      dir = ./.;
-    in
-    builtins.map (file: ./. + ("/" + file))
-      (builtins.filter (file: type: (type == "regular" && lib.hasSuffix ".nix" file) && (type != "directory")) (builtins.attrNames (builtins.readDir ./.)));
-}
+let
+  inherit (lib) makeExtensible attrValues foldr;
+  inherit (modules) mapModules;
 
-# let
-#   inherit (lib) makeExtensible attrValues foldr;
-#   inherit (modules) mapModules;
+  modules = import ./modules.nix {
+    inherit lib;
+    self.attrs = import ./attrs.nix { inherit lib; self = { }; };
+  };
 
-#   modules = import ./modules.nix {
-#     inherit lib;
-#     self.attrs = import ./attrs.nix { inherit lib; self = { }; };
-#   };
-
-#   mylib = makeExtensible (self:
-#     with self; mapModules ./.
-#       (file: import file { inherit self lib pkgs inputs; }));
-# in
-# mylib.extend
-#   (self: super:
-#     foldr (a: b: a // b) { } (attrValues super))
+  mylib = makeExtensible (self:
+    with self; mapModules ./.
+      (file: import file { inherit self lib pkgs inputs; }));
+in
+mylib.extend
+  (self: super:
+    foldr (a: b: a // b) { } (attrValues super))
