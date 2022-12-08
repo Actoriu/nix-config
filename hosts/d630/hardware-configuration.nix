2753ca4c21e6d2dc1ea15076f74a124fc05e9c58
Actoriu <51398008+Actoriu@users.noreply.github.com>
2022-12-08 12:01:10 +0800

    Update hardware-configuration.nix

--- hosts/d630/hardware-configuration.nix
+++ hosts/d630/hardware-configuration.nix
@@ -22,7 +22,7 @@
     # kernelModules = [ "wl" ];
     kernelModules = [ ];
     # kernelPackages = pkgs.linuxPackages_latest;
-    kernelPackages = pkgs.linuxKernel.packages.linux_5_4;
+    kernelPackages = pkgs.linuxKernel.packages.linux_5_15;
     supportedFilesystems = [ "btrfs" "exfat" "ntfs" "vfat" ];
     tmpOnTmpfs = true;
   };
