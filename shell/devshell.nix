b687fc6188f0bf8405e2ddae0aa4eab111c8934e
Actoriu <864149939@qq.com>
2022-12-25 17:32:07 +0800

    update

---
+++ shell/devshell.nix
@@ -0,0 +1,31 @@
+{ pkgs }:
+
+{
+  pkgs.devshell.mkShell {
+    name = "nix-config";
+    imports = [ (pkgs.devshell.extraModulesDir + "/git/hooks.nix") ];
+    git.hooks.enable = true;
+    git.hooks.pre-commit.text = "${pkgs.treefmt}/bin/treefmt";
+    packages = with pkgs; [
+      cachix
+      nix-build-uncached
+      nixpkgs-fmt
+      nodePackages.prettier
+      nodePackages.prettier-plugin-toml
+      nvfetcher
+      shfmt
+      treefmt
+    ];
+    commands = [
+      {
+        category = "update";
+        name = pkgs.nvfetcher.pname;
+        help = pkgs.nvfetcher.meta.description;
+        command = "cd $PRJ_ROOT/pkgs; ${pkgs.nvfetcher}/bin/nvfetcher -c ./sources.toml $@";
+      }
+    ];
+    devshell.startup.nodejs-setuphook = pkgs.lib.stringsWithDeps.noDepEntry ''
+      export NODE_PATH=${pkgs.nodePackages.prettier-plugin-toml}/lib/node_modules:$NODE_PATH
+    '';
+  };
+}
