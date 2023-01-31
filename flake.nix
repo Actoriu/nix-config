2225775208e1dbf6923434ea2e8ff5dcb3c2ece5
Actoriu <864149939@qq.com>
2023-01-31 16:03:19 +0800

    update

--- flake.nix
+++ flake.nix
@@ -170,9 +170,9 @@
       #   import ./pkgs { inherit pkgs; }
       # );
 
-      devShells = forEachSystem (system: {
+      devShells = {
         default = import ./shell/default.nix { inherit pkgs; };
-      });
+      };
 
       nixosConfigurations = {
         d630 = nixpkgs.lib.nixosSystem {
