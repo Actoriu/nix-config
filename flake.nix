f263b2bbfbe6122e1f612954b4ae40d4954954cd
Actoriu <864149939@qq.com>
2023-01-31 17:57:29 +0800

    update

--- flake.nix
+++ flake.nix
@@ -114,10 +114,10 @@
 
       forEachSystem = nixpkgs.lib.genAttrs [ "aarch64-linux" "x86_64-linux" ];
 
-      lib = nixpkgs.lib.extend (self: super: {
+      lib = nixpkgs.lib.extend (final: prev: {
         my = import ./lib {
           inherit inputs pkgs;
-          lib = self;
+          lib = final;
         };
       });
 
@@ -135,7 +135,7 @@
         };
       });
     in {
-      lib = lib.my;
+      # lib = lib.my;
 
       overlays = {
         # default = import ./overlays { inherit inputs; };
