3dee80786bbc7a44e0840e0b4c2f5aee464fdbc4
Actoriu <864149939@qq.com>
2023-02-01 18:35:52 +0800

    update

--- lib/default.nix
+++ lib/default.nix
@@ -4,7 +4,7 @@
 #   lib = lib.makeExtensible (self: let
 #     callLibs = file: import file { lib = self; };
 #   in
-rec {
+{
   droid = import ./droid.nix { inherit lib inputs; };
   home = import ./home.nix { inherit lib inputs; };
   nixos = import ./nixos.nix { inherit lib inputs; };
