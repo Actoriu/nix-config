b1ba4161788a672a6a75477312a2c7d6bbb6a96e
Actoriu <864149939@qq.com>
2023-02-01 17:45:39 +0800

    update

--- flake.nix
+++ flake.nix
@@ -183,7 +183,7 @@
         });
 
       nixosConfigurations = {
-        d630 = lib.my.mkNixosConfig {
+        d630 = lib.my.nixos.mkNixosConfig {
           extraModules = [
             ({ ... }: {
               nixpkgs = {
