1d1e9393031b215f03509d7f006bca74a192931c
Actoriu <864149939@qq.com>
2023-02-02 17:55:21 +0800

    formatter

--- flake.nix
+++ flake.nix
@@ -141,7 +141,7 @@
     #   pre-commit-check = pre-commit-hooks.lib.${system}.run {
     #     src = ./.;
     #     hooks = {
-    #       nixpkgs-fmt.enable = true;
+    #       alejandra.enable = true;
     #     };
     #   };
     # });
