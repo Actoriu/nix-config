aac7b0ee7715df1e769ee9f0e9bb380890385117
Actoriu <864149939@qq.com>
2023-02-02 17:00:30 +0800

    update

--- lib/default.nix
+++ lib/default.nix
@@ -3,15 +3,8 @@
 , ...
 }:
 
-# let
 {
   droid = import ./droid.nix { inherit lib inputs; };
   home = import ./home.nix { inherit lib inputs; };
   nixos = import ./nixos.nix { inherit lib inputs; };
 }
-  # in
-  # {
-  #   inherit (droid) mkDroidConfig;
-  #   inherit (home) mkHomeConfig;
-  #   inherit (nixos) mkNixosConfig;
-  # }
