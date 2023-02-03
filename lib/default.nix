50953513150537d79da74c9bd60a3fa55f3e107d
Actoriu <864149939@qq.com>
2023-02-03 16:29:38 +0800

    update

--- lib/default.nix
+++ lib/default.nix
@@ -3,10 +3,12 @@
   lib,
   inputs,
 } @ args:
-with {inherit (lib) makeExtensible attrValues foldr;};
+with {inherit (lib) makeExtensible attrValues foldr;}; let
+  directory = builtins.filterSource (path: type: !(type == "directory" && baseNameOf path == "compat")) ./.;
+in
   (makeExtensible (final:
     with final;
-      (import ./map.nix args).modules builtins.filterSource (path: type: !(type == "directory" && baseNameOf path == "compat")) ./. (file: import file args)))
+      (import ./map.nix args).modules directory (file: import file args)))
   .extend
   (final: prev: foldr (x: y: x // y) {} (attrValues prev))
 # {
