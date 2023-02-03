9e0a4ea9f5abfb0c973689c46f56b6e619be4245
Actoriu <864149939@qq.com>
2023-02-03 15:40:19 +0800

    update

--- lib/default.nix
+++ lib/default.nix
@@ -1,9 +1,21 @@
+# {
+#   lib,
+#   inputs,
+#   ...
+# }:
+# {
+#   droid = import ./droid.nix {inherit lib inputs;};
+#   home = import ./home.nix {inherit lib inputs;};
+#   nixos = import ./nixos.nix {inherit lib inputs;};
+# }
 {
+  self,
   lib,
   inputs,
-  ...
-}: {
-  droid = import ./droid.nix {inherit lib inputs;};
-  home = import ./home.nix {inherit lib inputs;};
-  nixos = import ./nixos.nix {inherit lib inputs;};
-}
+} @ args:
+with {inherit (lib) makeExtensible attrValues foldr;};
+  (makeExtensible (final:
+    with final;
+      (import ./map.nix args).modules ./. (file: import file args)))
+  .extend
+  (final: prev: foldr (x: y: x // y) {} (attrValues prev))
