cce606c3c9fa5b91d141f625b8acdbb46d0169c8
Actoriu <864149939@qq.com>
2023-02-04 16:58:41 +0800

    update

--- lib/default.nix
+++ lib/default.nix
@@ -1,8 +1,13 @@
 {
   lib,
   inputs,
-}: {
-  droid = import ./droid.nix {inherit lib inputs;};
-  home = import ./home.nix {inherit lib inputs;};
-  nixos = import ./nixos.nix {inherit lib inputs;};
+  outputs,
+}:
+let
+version = lib.fileContents ./.version;
+  in
+{
+  droid = import ./droid.nix {inherit lib inputs outputs version;};
+  home = import ./home.nix {inherit lib inputs outputs version;};
+  nixos = import ./nixos.nix {inherit lib inputs outputs version;};
 }
