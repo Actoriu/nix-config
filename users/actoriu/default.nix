25a1042e200894de58f379ccbee2da2d7ee38082
Actoriu <51398008+Actoriu@users.noreply.github.com>
2023-04-01 22:02:50 +0800

    update

--- users/actoriu/default.nix
+++ users/actoriu/default.nix
@@ -10,7 +10,7 @@
     cc.enable = true;
     javascript.enable = true;
     python.enable = true;
-    texlive.enable = true;
+    # texlive.enable = true;
     # editors
     emacs = {
       enable = true;
