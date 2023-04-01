7c9e34fcf153be68ed972e701f962b2016d54d18
Actoriu <51398008+Actoriu@users.noreply.github.com>
2023-04-01 22:07:58 +0800

    update linuxkernel 5.15

--- hosts/nixos/d630/hardware-configuration.nix
+++ hosts/nixos/d630/hardware-configuration.nix
@@ -23,7 +23,7 @@
     # kernelModules = [ "wl" ];
     kernelModules = [];
     # kernelPackages = pkgs.linuxPackages_latest;
-    kernelPackages = pkgs.linuxKernel.packages.linux_6_1;
+    kernelPackages = pkgs.linuxKernel.packages.linux_5_15;
     supportedFilesystems = ["btrfs" "exfat" "ntfs" "vfat"];
     tmpOnTmpfs = true;
   };
