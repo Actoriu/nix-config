e9f38f671a048f081e458275f9d412bcca0b8620
Actoriu <51398008+Actoriu@users.noreply.github.com>
2023-04-28 16:06:28 +0800

    update

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Nix configuration with flakes";
   /*
   nixConfig = {
-    extra-experimental-features = "nix-command flakes repl-flake";
+    extra-experimental-features = "nix-command flakes";
     substituters = [
       "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
       "https://mirrors.ustc.edu.cn/nix-channels/store"
--- hosts/droid/oneplus5/default.nix
+++ hosts/droid/oneplus5/default.nix
@@ -17,7 +17,17 @@
       "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
       "https://mirrors.ustc.edu.cn/nix-channels/store"
       "https://cache.nixos.org/"
+      "https://nix-community.cachix.org"
+      "https://pre-commit-hooks.cachix.org"
+      "https://nix-actions.cachix.org"
       "https://nix-on-droid.cachix.org"
+    ];
+    trustedPublicKeys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "pre-commit-hooks.cachix.org-1:Pkk3Panw5AW24TOv6kz3PvLhlH8puAsJTBbOPmBo7Rc="
+      "nix-actions.cachix.org-1:WTp8/9EIjoPRzwSERLLMHzDUVGthajaIJ/zEZY6DHvM="
+      "nix-on-droid.cachix.org-1:56snoMJTXmDRC1Ei24CmKoUqvHJ9XCp+nidK7qkMQrU="
     ];
   };
 
