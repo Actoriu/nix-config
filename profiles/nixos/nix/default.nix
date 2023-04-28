bc7f674a35717b976ede72b04a0767d8d555b9a4
Actoriu <51398008+Actoriu@users.noreply.github.com>
2023-04-28 16:06:28 +0800

    update

--- profiles/nixos/nix/default.nix
+++ profiles/nixos/nix/default.nix
@@ -7,24 +7,22 @@
   imports = [./cachix];
 
   nix = {
-    extraOptions = ''
-      experimental-features = nix-command flakes
-    '';
-
+    /*
     gc = {
       automatic = true;
       options = ''--delete-older-than 7d'';
     };
 
     optimise.automatic = true;
+    */
 
     settings = {
       allowed-users = ["@wheel"];
       auto-optimise-store = true;
+      experimental-features = ["nix-command" "flakes"];
       fallback = true;
       keep-outputs = true;
       keep-derivations = true;
-      min-free = 1024 * 1024 * 1024;
       sandbox = true;
       system-features = ["nixos-test" "benchmark" "big-parallel" "kvm"];
       trusted-users = ["root" "@wheel"];
