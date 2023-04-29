f2d7209fa1ef14967d73e365ad9807ab25944e7f
Actoriu <51398008+Actoriu@users.noreply.github.com>
2023-04-29 20:34:41 +0800

    update

--- profiles/nixos/nix/cachix/nix-actions.nix
+++ profiles/nixos/nix/cachix/nix-actions.nix
@@ -3,7 +3,7 @@
     settings = {
       substituters = ["https://nix-actions.cachix.org"];
       trusted-public-keys = [
-        "nix-actions.cachix.org-1:WTp8/9EIjoPRzwSERLLMHzDUVGthajaIJ/zEZY6DHvM="
+        "nix-actions.cachix.org-1:0QFVMkhyCHpNBpxW1XyI8/+OGW81Vt1KpeeN/IdjEYg="
       ];
     };
   };
