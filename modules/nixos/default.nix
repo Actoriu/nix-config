36f5cf85f3c06e3a2117a22e0f7c804e0fd7d043
Actoriu <51398008+Actoriu@users.noreply.github.com>
2023-05-07 16:01:33 +0800

    update modules

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,16 +1,16 @@
 {
-  fonts = import ./fonts.nix;
+  fonts = import ./fonts;
   # hardware
-  audio = import ./hardware/audio.nix;
-  bluetooth = import ./hardware/bluetooth.nix;
-  amd = import ./hardware/cpu/amd.nix;
-  intel = import ./hardware/cpu/intel.nix;
-  opengl = import ./hardware/opengl.nix;
-  printers = import ./hardware/printers.nix;
-  nvidia = import ./hardware/video/nvidia.nix;
-  loader = import ./loader.nix;
-  locale = import ./locale.nix;
-  network = import ./network.nix;
-  power-management = import ./power-management.nix;
-  users = import ./users.nix;
+  audio = import ./hardware/audio;
+  bluetooth = import ./hardware/bluetooth;
+  amd = import ./hardware/cpu/amd;
+  intel = import ./hardware/cpu/intel;
+  opengl = import ./hardware/opengl;
+  printers = import ./hardware/printers;
+  nvidia = import ./hardware/video/nvidia;
+  loader = import ./loader;
+  locale = import ./locale;
+  network = import ./network;
+  power-management = import ./power-management;
+  users = import ./users;
 }
